08295cae8e3557b27193073d083bc067fd78d9ab
ishanpm <ishanpm@gmail.com>
2017-07-26 23:09:26 -0500

    added mouse input and moving creature

--- color-ai/ai.js
+++ color-ai/ai.js
@@ -23,7 +23,45 @@ class CanvasWrapper {
         this.offsetx = this.width/2;
         this.offsety = this.height/2;
         this.scale = 1;
+        this.onClick = null;
+        this.mouseOver = false;
+        this.mousePos = [0,0];
+        
+        var that = this;
+        
+        this.canvas.addEventListener("click", function(e) {
+            var x = e.pageX - e.target.offsetLeft;
+            var y = e.pageY - e.target.offsetTop;
+            
+            if (that.onClick) {
+                that.onClick(that.fromScreenX(x), that.fromScreenY(y));
+            }
+        })
+        
+        this.canvas.addEventListener("mousemove", function(e) {
+            var x = e.pageX - e.target.offsetLeft;
+            var y = e.pageY - e.target.offsetTop;
+            
+            that.mousePos = [that.fromScreenX(x), that.fromScreenY(y)];
+        })
+        
+        this.canvas.addEventListener("mouseenter", function(e) {
+            that.mouseOver = true;
+        })
+        
+        this.canvas.addEventListener("mouseleave", function(e) {
+            that.mouseOver = false;
+        })
     }
+    // Coordinate transformations
+    toScreenX(val) {return (this.offsetx + val) * this.scale}
+    toScreenY(val) {return (this.offsety - val) * this.scale}
+    toScreenScale(val) {return val * this.scale}
+    fromScreenX(val) {return val / this.scale - this.offsetx}
+    fromScreenY(val) {return this.offsety - val / this.scale}
+    fromScreenScale(val) {return val / this.scale}
+                          
+    // Graphics
     fill(color) {
         this.ctx.fillStyle = `rgb(${Math.round(color[0]*255)},${Math.round(color[1]*255)},${Math.round(color[2]*255)})`;
         this.ctx.fillRect(0,0,this.width,this.height);
@@ -31,10 +69,11 @@ class CanvasWrapper {
     fillCircle(color, radius, x, y) {
         this.ctx.beginPath();
         this.ctx.fillStyle = `rgb(${Math.round(color[0]*255)},${Math.round(color[1]*255)},${Math.round(color[2]*255)})`;
-        this.ctx.arc(this.offsetx+x, this.offsety-y, this.scale*radius, 0, Math.PI*2);
+        this.ctx.arc(this.toScreenX(x), this.toScreenY(y), this.toScreenScale(radius), 0, Math.PI*2);
         this.ctx.closePath();
         this.ctx.fill();
     }
+    
 }
 
 class Board {
@@ -42,9 +81,9 @@ class Board {
         this.params = {
             creatureAwareness: 10,
             creatureSpeed: 1/20,
-            movementCost: 0.2,
+            movementCost: 1/10000,
             eatSpeed: 5,
-            drag: 19/20
+            drag: 15/20
         }
         this.creatures = [];
     }
@@ -106,20 +145,25 @@ class Creature extends Thing {
     }
     think() {
         var sensors = this.board.sense(this, this.board.params.creatureAwareness, this.type)
-        this.thoughts = this.mind.think(this.energy, this.vx, this.vy, sensors);
+        this.thoughts = this.mind.think(this.energy, this.vx, this.vy, sensors, this);
+        
+        this.thoughts.moveX = +this.thoughts.moveX || 0;
+        this.thoughts.moveY = +this.thoughts.moveY || 0;
     }
     update() {
-        this.vx += this.thoughts.moveX * this.board.params.creatureSpeed;
+        this.vx += +this.thoughts.moveX * this.board.params.creatureSpeed;
         this.x += this.vx;
         this.vx *= this.board.params.drag;
         
-        this.vy += this.thoughts.moveY * this.board.params.creatureSpeed;
+        this.vy += +this.thoughts.moveY * this.board.params.creatureSpeed;
         this.y += this.vy;
         this.vy *= this.board.params.drag;
       
         this.radius = Math.sqrt(this.energy);
         
         this.color = hsv2rgb(this.thoughts.hue + this.type * 120, this.thoughts.sat, this.thoughts.val);
+        
+        this.energy -= (Math.pow(this.thoughts.moveX, 2) + Math.pow(this.thoughts.moveY, 2)) * this.board.params.movementCost
     }
     collide(other) {
         if (other instanceof Creature) {
@@ -139,7 +183,7 @@ class Creature extends Thing {
 
 class DummyMind {
     constructor() {}
-    think(energy, vx, vy) {
+    think(energy, vx, vy, sensors) {
         return {
             moveX: 0,
             moveY: 0,
@@ -151,31 +195,39 @@ class DummyMind {
     }
 }
 
+class FollowMind {
+    constructor() {}
+    think(energy, vx, vy, sensors, creature) {
+        return {
+            moveX: canvas.mousePos[0] - creature.x,
+            moveY: canvas.mousePos[1] - creature.y,
+            hue: 0,
+            sat: 1,
+            val: 1,
+            respawn: 0
+        }
+    }
+}
+
 var board;
-var creature;
 var canvas;
 var repeatingTick;
+var minds = {dummy:_=>new DummyMind(), follow:_=>new FollowMind()}
 
 function init() {
     board = new Board();
     board.canvas = new CanvasWrapper();
-    creature = newCreature(0, 0, 0);
     canvas = new CanvasWrapper();
     repeatingTick = setInterval(tick, 50);
+    
+    canvas.onClick = function(x,y) {
+        type = +document.getElementById("type").value;
+        mind =  document.getElementById("mind").value;
+        new Creature(board, x, y, minds[mind](), type, 100);
+    }
 }
 
 function tick() {
     board.tick();
     board.draw(canvas);
-}
-
-function newCreature(x, y, type) {
-    return new Creature(board, x, y, new DummyMind(), type, 100);
-}
-    
-function clickAdd() {
-    var x   =+document.getElementById("x").value,
-        y   =+document.getElementById("y").value,
-        type=+document.getElementById("type").value
-    newCreature(x, y, type);
 }
