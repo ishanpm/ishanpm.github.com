e0533e72a85a037b5b786f1fe5e523b82365cf8b
ishanpm <ishanpm@gmail.com>
2018-08-23 19:08:53 -0500

    added other transformations

--- math/main.js
+++ math/main.js
@@ -4,33 +4,93 @@ angular.module("AppNameHere", [])
 .controller("MainController", ["$scope", function($scope) {
   $scope.x = "two plus two equals four ({\u03bb {\u03bb @<2>}} means true)";
   
+  $scope.exprControl = null;
+  
+  window.mainScope = $scope;
+  
   $scope.expr1 = [example.parseLambda(example.twotwofour)];
   $scope.input = example.twotwofour;
+  $scope.showGoButton = false;
+  $scope.showClearButton = false;
+  $scope.keepExprs = false;
+  $scope.rules = Object.keys(example.rules);
+  $scope.selectedRule = $scope.rules[0];
   $scope.selection = [];
   
-  console.log($scope.expr1);
+  $scope.rules.unshift("delete", "clone");
+  
+  $scope.$watch("selectedRule", function() {
+    if ($scope.selectedRule == "delete") {
+      $scope.showClearButton = false;
+      $scope.showGoButton = false;
+      $scope.clearSelection();
+      return;
+    }
+    var rule = example.rules[$scope.selectedRule];
+    $scope.showClearButton = (rule.inputsNeeded != 1);
+    $scope.showGoButton = (rule.inputsNeeded == "many");
+    $scope.clearSelection();
+  });
+  
+  $scope.clearSelection = function() {
+    $scope.selection = [];
+    $scope.exprControl.setAllSelected(false);
+  }
   
   $scope.setExprLambda = function setExprLambda(val) {
-    $scope.expr1 = [example.parseLambda(val)];
-  }  
+    $scope.expr1 = example.parseMultiLambda(val);
+  }
+  $scope.appendExprLambda = function setExprLambda(val) {
+    $scope.expr1 = $scope.expr1.concat(example.parseMultiLambda(val));
+  }
+  
   $scope.onExprClick = function onExprClick(val) {
-    var expr = logic.index($scope.expr1, val)
-    if (expr.type == "lambda") {
-      val = val.slice(0,-1);
-    } else if (expr[0] && expr[0].type == "lambda") {
-      // No change
-    } else if (expr[0] && expr[0][0] && expr[0][0].type == "lambda") {
-      val = val.concat([0]);
-    } else {
+    if ($scope.selectedRule == "delete") {
+      if (val.length > 0) {
+        $scope.expr1.splice(val[0], 1);
+      }
+      $scope.clearSelection()
+      return;
+    } else if ($scope.selectedRule == "clone") {
+      if (val.length> 0) {
+        $scope.expr1.push($scope.expr1[val[0]]);
+      }
+      $scope.clearSelection();
       return;
     }
     
-    var a = [[val.slice(1).slice(0,-1)]];
-    var b = [logic.getAllFree(logic.index($scope.expr1, val)[1])];
-    var newExpr = logic.apply($scope.expr1, example.lambda, [a, b, []]);
-    if (newExpr.length > 0) {
-      $scope.expr1 = newExpr;
+    var rule = example.rules[$scope.selectedRule];
+    
+    $scope.selection.push(val);
+    
+    if ($scope.selection.length == rule.inputsNeeded) {
+      $scope.doTransform();
     }
+  }
+  
+  $scope.doTransform = function doTransform() {
+    var rule = example.rules[$scope.selectedRule];
+    
+    var expr = $scope.selection.map(e => logic.index($scope.expr1, e));
+    
+    var input = rule.getVars($scope.expr1, $scope.selection, expr);
+    
+    if (input) {
+      var exprs = $scope.selection.slice(0,rule.ante.length).map(e => $scope.expr1[e[0]]);
+      var result = logic.apply(exprs, rule, input);
+
+      if (result.length > 0) {
+        //$scope.expr1 = result;
+        $scope.expr1 = $scope.expr1.concat(result);
+        
+        if (!$scope.keepExprs) {
+          var remove = $scope.selection.map(e=>e[0]);
+          $scope.expr1 = $scope.expr1.filter((e,i)=>remove.indexOf(i) == -1);
+        }
+      }
+    }
+    
+    $scope.clearSelection();
   }
   
   setExpr = function(val) {
@@ -59,16 +119,16 @@ angular.module("AppNameHere", [])
 .component("expression", {
   bindings: {
     expression: '=',
+    exprControl: '&',
     exprClick: '&',
-    doClick: '&',
   },
   template: `
     <div ng-if="!logic.isAtomic($ctrl.expression)" ng-click="onclick($event)" ng-class="{selected:selected, expr:true, hover:hover && !childHover}">
       <expression
-        ng-init="doChildClick[$index]=1"
-        ng-repeat="e in $ctrl.expression"
-        expression="e" expr-click="subclick($index, expr)"
-        do-click="doChildClick($index,doClick)"
+        ng-repeat="e in $ctrl.expression track by $index"
+        expression="e"
+        expr-click="subclick($index, expr)"
+        expr-control="setChildControl(ctrl, $index)"
         ng-mouseenter="setChildHover(true)"
         ng-mouseleave="setChildHover(false)"></expression>
     </div>
@@ -77,27 +137,23 @@ angular.module("AppNameHere", [])
   controller: function($scope, $element) {
     var ctrl = this;
     
-    function doClick() {
-      //$scope.selected = !$scope.selected;
-      
-      for (var i=0; i<$scope.childClickListener.length; i++) {
-        $scope.childClickListener[i]();
-      }
-    }
-    
+    $scope.children = [];
     $scope.logic = logic;
     $scope.selected = false;
     $scope.childClickListener = [];
     $scope.hover = false;
     $scope.childHover = false;
     
+    $scope.setChildControl = function(childController, index) {
+      $scope.children[index] = childController;
+    }
+    
     $scope.setChildHover = function(val) {
       $scope.childHover = val;
     };
     
     $scope.onclick = function(event) {
-      //$scope.selected = !$scope.selected;
-      doClick();
+      $scope.selected = true;
       event.stopPropagation();
       ctrl.exprClick({expr:[]});
     }
@@ -107,8 +163,15 @@ angular.module("AppNameHere", [])
       ctrl.exprClick({expr: expr});
     }
     
-    $scope.doChildClick = function(index, doClick) {
-      $scope.childClickListener[index] = doClick;
+    ctrl.setSelected = function(val) {
+      $scope.selected = val;
+    }
+    
+    ctrl.setAllSelected = function(val) {
+      $scope.selected = val;
+      for (var i=0; i<$scope.children.length; i++) {
+        $scope.children[i].setAllSelected(val);
+      }
     }
     
     $element.on("mouseenter", function(e) {
@@ -121,7 +184,7 @@ angular.module("AppNameHere", [])
       $scope.$apply();
     })
     
-    ctrl.doClick({doClick})
+    ctrl.exprControl({ctrl});
   }
 })
 
